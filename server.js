6c6d8900aea6a5a3737c5d5abdb367bc21035556
JoeVictor <joaoriolima@gmail.com>
2023-02-28 20:14:38 +0800

    handlebars update

---
+++ server.js
@@ -0,0 +1,41 @@
+// DEPENDENCIES :
+
+// const express = require('express');
+// const exphbs = require('express-handlebars');
+//const path = require('path');
+
+
+
+// EXPRESS APP : 
+
+// const helpers = require('./utils/helpers');
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// const hbs = exphbs.create({ helpers });
+
+
+
+
+// Setting the handlebars as the default template engine : 
+
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+
+
+// app.use(express.static(path.join(_dirname, 'public')));
+
+// ROUTES :
+
+// app.use(require('./controllers/index.js'));
+
+
+// Making the server to listen : 
+
+// app.listen(PORT, () => {
+//   console.log('Server listening on:' + PORT);
+// });
+
+
+//
