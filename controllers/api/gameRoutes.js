df8d9e603b764aea92f49bf9217f2a1cc14b0035
Harry_Desktop <hward.1508@gmail.com>
2023-02-28 19:45:24 +0800

    starting framework for api-routes

---
+++ controllers/api/gameRoutes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const { Game } = require('../models');
+
+router.get('/', async (req, res) => {
+    const games = await Game.findAll();
+    return games;
+});
+
+module.exports = router;
---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/login', async (req, res) => {
+  try {
+    // finds the user that matchs the email entered
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    // calls the checkpassword function and waits for the response
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    // if all has passed, saves the login details in the session
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    // if the use if logged in, then logout, if not then send 404 error
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
