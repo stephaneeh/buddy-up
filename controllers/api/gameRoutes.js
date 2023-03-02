d7f39f5a46eb007c541697f5c227d9efcf65a4e7
Harry_Desktop <hward.1508@gmail.com>
2023-03-02 21:40:53 +0800

    fixing routing bug fixes

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -2,8 +2,17 @@ const router = require('express').Router();
 const { Game } = require("../../models");
 
 router.get('/', async (req, res) => {
-    const games = await Game.findAll();
-    return games;
+    const dbGames = await Game.findAll();
+    // console.log(dbGames);
+    const games = dbGames.map((game) => (
+         game.get({plain: true})
+    ));
+
+    console.log(games);
+    res.render('homepage', {
+        games,
+        logged_in: req.session.logged_in,
+    });
 });
 
 module.exports = router;
