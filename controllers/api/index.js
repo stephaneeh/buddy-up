d8e2364ec94c0e8dcb83b4adc9107bbee83be595
Harry_Desktop <hward.1508@gmail.com>
2023-03-07 21:46:17 +0800

    implementing modals

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const gameRoutes = require('./gameRoutes');
 const requestRoutes = require('./requestRoutes');
 
 router.use('/users', userRoutes);
 
 router.use('/requests', requestRoutes);
 
-router.use('/games', gameRoutes);
 
 module.exports = router;
