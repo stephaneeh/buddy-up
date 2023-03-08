9356bb54da6c80200b4c42ffe1cd115ba9fdc47b
caoimhejyoti <caoimhejyoti@gmail.com>
2023-03-08 15:47:54 +0800

    commenting code to allow seed to work

--- models/user.js
+++ models/user.js
@@ -11,45 +11,44 @@ class User extends Model {
 User.init(
   {
     id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
     user_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-            isEmail: true,
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
       },
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [8],
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
       },
     },
-    game_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'game',
-        key: 'id',
-      },
-    },
-    console_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'console',
-        key: 'id',
-      },
-    }
-
+    // game_id: {
+    //   type: DataTypes.INTEGER,
+    //   references: {
+    //     model: 'game',
+    //     key: 'id',
+    //   },
+    // },
+    // console_id: {
+    //   type: DataTypes.INTEGER,
+    //   references: {
+    //     model: 'console',
+    //     key: 'id',
+    //   },
+    // }
   },
   {
     hooks: {
