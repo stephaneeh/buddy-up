0114febcb39fc320c5be53cfc420fbba13e08ef3
caoimhejyoti <caoimhejyoti@gmail.com>
2023-03-09 19:31:16 +0800

    prettier

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,11 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const requestRoutes = require('./requestRoutes');
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const requestRoutes = require("./requestRoutes");
 
-router.use('/users', userRoutes);
 
-router.use('/requests', requestRoutes);
+router.use("/users", userRoutes);
+
+router.use("/requests", requestRoutes);
 
 
 module.exports = router;
