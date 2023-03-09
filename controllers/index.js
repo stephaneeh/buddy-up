c365af0a45b0cc43f65d806040a45deded60c715
caoimhejyoti <caoimhejyoti@gmail.com>
2023-03-09 19:30:26 +0800

    adding find a buddy routes + initital code

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+const apiRoutes = require("./api");
+const homeRoutes = require("./homeRoutes");
+const findabuddyRoutes = require("./findabuddyRoutes");
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+router.use("/find-a-buddy", findabuddyRoutes);
 
 module.exports = router;
