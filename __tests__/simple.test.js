8960c01d88a9a34c61c5d9b9e098fdc4a3aa4cda
Stephanee Hayden <stephaneehayden@gmail.com>
2023-03-10 20:06:44 +1000

    update test suite

--- __tests__/simple.test.js
+++ __tests__/simple.test.js
@@ -1,27 +1,37 @@
 //NOT WORKING RIGHT NOW
 //FIXME: https://jaygould.co.uk/2020-07-28-jest-sequelize-testing-dedicated-database/
+//FIXME: https://www.makeuseof.com/express-apis-jest-test/
 
-const request = require("supertest");
-const Sequelize = require("sequelize");
-const app = require("../server");
+// const request = require("supertest");
+// const baseURL = "http://localhost:3001";
+// const crypto = require("crypto");
 
-require("dotenv").config();
+// describe("GET /todos", () => {
+//   const newTodo = {
+//     id: crypto.randomUUID(),
+//     item: "Drink water",
+//     completed: false,
+//   };
+//   beforeAll(async () => {
+//     // set up the todo
+//     await request(baseURL).post("/todo").send(newTodo);
+//   });
+//   afterAll(async () => {
+//     await request(baseURL).delete(`/todo/${newTodo.id}`);
+//   });
+//   it("should return 200", async () => {
+//     const response = await request(baseURL).get("/todos");
+//     expect(response.statusCode).toBe(200);
+//     expect(response.body.error).toBe(null);
+//   });
+//   it("should return todos", async () => {
+//     const response = await request(baseURL).get("/todos");
+//     expect(response.body.data.length >= 1).toBe(true);
+//   });
+// });
 
 describe("Initial test", () => {
   it("1+1=2", () => {
     expect(1 + 1).toEqual(2);
-  });
-});
-
-/* Connecting to the database before each test. */
-beforeAll(async () => {
-  await thisDb.sequelize.sync({ force: true });
-});
-
-describe("GET /", () => {
-  it("should return all games", async () => {
-    const res = await request(app).get("/");
-    expect(res.statusCode).toBe(200);
-    expect(res.body.length).toBeGreaterThan(0);
   });
 });
